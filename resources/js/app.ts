8e381b8def0b1b3f111eca3faf3e95c651bac6ef
AceRex <ace.rexnoir@gmail.com>
2024-06-20 10:07:01 +0530

    setup : set default layout

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,11 +1,14 @@
 import "./bootstrap";
 import { DefineComponent, createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
+import MainLayout from "./Layout/MainLayout.vue";
 
 createInertiaApp({
     resolve: async (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
         const page = (await pages[`./Pages/${name}.vue`]) as DefineComponent;
+        page.default.layout = page.default.layout || MainLayout;
+
         return page;
     },
     setup({ el, App, props, plugin }) {
