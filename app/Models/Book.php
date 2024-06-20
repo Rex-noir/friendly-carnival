465987e41e972b12ff4955337dfd78d75739c267
AceRex <ace.rexnoir@gmail.com>
2024-06-20 21:41:23 +0530

    feat : add relationship to genres table

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
@@ -21,5 +22,10 @@ class Book extends Model
     public function author(): BelongsTo
     {
         return $this->belongsTo(Author::class);
+    }
+
+    public function genres(): BelongsToMany
+    {
+        return $this->belongsToMany(Genre::class, 'book_genre');
     }
 }
