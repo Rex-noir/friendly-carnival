42535502abb5bc74f04299bb56a97b880a1de67b
AceRex <ace.rexnoir@gmail.com>
2024-06-21 13:55:54 +0530

    feat : integrate relationship with roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -45,8 +46,8 @@ class User extends Authenticatable
         ];
     }
 
-    public function isAdmin()
+    public function roles(): BelongsToMany
     {
-        return $this->is_admin;
+        return $this->belongsToMany(Role::class, 'role_user');
     }
 }
