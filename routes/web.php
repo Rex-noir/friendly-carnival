b071cfc29054dbb02d277fa2a029c0ad54f5b761
AceRex <ace.rexnoir@gmail.com>
2024-06-21 18:09:25 +0530

    feat : add routes for login pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return inertia('Index');
 });
+
+Route::prefix('auth')->group(function () {
+    Route::inertia('/', 'Auth/Login');
+    Route::inertia('login', 'Auth/Login');
+    Route::inertia('signup', 'Auth/Signup');
+});
