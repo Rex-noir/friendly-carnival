c89f318661b6902142990d992176a1286777370a
AceRex <ace.rexnoir@gmail.com>
2024-06-22 09:08:55 +0530

    feat : error msg, change : username to email

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -3,26 +3,25 @@ import { useForm } from "@inertiajs/vue3";
 import InputText from "primevue/inputtext";
 import FloatLabel from "primevue/floatlabel";
 import Password from "primevue/password";
-import password from "@/presets/aura/password";
 import Button from "primevue/button";
 import Checkbox from "primevue/checkbox";
+import InlineMessage from "primevue/inlinemessage";
 
 const form = useForm({
-    username: null,
+    email: null,
     password: null,
     remember: false,
 });
 </script>
 <template>
     <form @submit.prevent="form.post('/auth/login/submit')">
-        <div class="grid place-content-center mt-4 gap-6">
+        <div class="grid place-content-center mt-4 gap-7">
+            <InlineMessage v-show="form.errors.email" severity="error">{{
+                form.errors.email
+            }}</InlineMessage>
             <FloatLabel>
-                <InputText
-                    class="w-full"
-                    id="username"
-                    v-model="form.username"
-                />
-                <label for="username">Username</label>
+                <InputText class="w-full" id="username" v-model="form.email" />
+                <label for="username">Email</label>
             </FloatLabel>
             <FloatLabel>
                 <Password id="password" v-model="form.password" toggleMask />
@@ -35,7 +34,7 @@ const form = useForm({
                     name="remember"
                     value="remember"
                 />
-                <label for="ingredient1" class="ml-2">Rememer? </label>
+                <label for="ingredient1" class="ml-2"> Remember me. </label>
             </div>
 
             <Button
