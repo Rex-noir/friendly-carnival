cff02ca86160142f2a17242dd778ce899d255061
AceRex <ace.rexnoir@gmail.com>
2024-06-23 10:53:19 +0530

    feat : add background transition to make it smoother

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
     @inertiaHead
 </head>
 
-<body class="min-h-screen dark:bg-surface-900">
+<body class="min-h-screen dark:bg-surface-900 transition-[background] duration-500  dark:text-white">
     @inertia
 </body>
 
