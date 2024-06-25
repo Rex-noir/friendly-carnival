577156c426f8324ca4f5a33ee57f620b815fc4b4
AceRex <ace.rexnoir@gmail.com>
2024-06-26 01:09:38 +0530

    feat : add tab menu

--- resources/js/Pages/Dashboard/Admin/Users.vue
+++ resources/js/Pages/Dashboard/Admin/Users.vue
@@ -1,2 +1,27 @@
-<script setup lang="ts"></script>
-<template>Hello</template>
+<script setup lang="ts">
+import TabMenu from "primevue/tabmenu";
+import { ref } from "vue";
+
+const active = ref(0);
+
+const items = ref([
+    { label: "Users", icon: "pi pi-home" },
+    { label: "Authors", icon: "pi pi-chart-line" },
+    { label: "Admin", icon: "pi pi-list" },
+]);
+</script>
+<template>
+    <div>
+        <TabMenu v-model:activeIndex="active" :model="items">
+            <template #item="{ item, props }">
+                <a
+                    v-ripple
+                    v-bind="props.action"
+                    class="flex items-center gap-2"
+                >
+                    <span class="font-bold">{{ item.label }}</span>
+                </a>
+            </template>
+        </TabMenu>
+    </div>
+</template>
