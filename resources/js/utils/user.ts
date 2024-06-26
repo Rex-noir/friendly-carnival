dd50f99a4a89fc4700fd1bbfa757a1645a942294
AceRex <ace.rexnoir@gmail.com>
2024-06-26 22:24:45 +0530

    feat : add utils for user

---
+++ resources/js/utils/user.ts
@@ -0,0 +1,24 @@
+import { User } from "@/types/users.interface";
+import axios, { AxiosResponse } from "axios";
+
+export default class UserUtils {
+    /**
+     *
+     * @param value The search term (string | number)
+     * @returns A promise that resolves to an AxiosResponse object containing the result
+     */
+    static async searchUser(value: string | number): Promise<User[]> {
+        try {
+            const response = await axios.post("/users/search", {
+                value,
+            });
+            if (response.status !== 200) {
+                throw new Error(response.statusText);
+            }
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching users", error);
+            throw error;
+        }
+    }
+}
