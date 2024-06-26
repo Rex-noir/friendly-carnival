3799b063aaa09e177d418fde3e9aa747b91e3993
AceRex <ace.rexnoir@gmail.com>
2024-06-26 12:37:52 +0530

    feat : finished initial design

--- resources/js/Pages/Dashboard/Admin/Users.vue
+++ resources/js/Pages/Dashboard/Admin/Users.vue
@@ -1,27 +1,40 @@
 <script setup lang="ts">
-import TabMenu from "primevue/tabmenu";
-import { ref } from "vue";
+import { Link, usePage } from "@inertiajs/vue3";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import { Pagination, User } from "@/types/users.interface";
+import { onMounted, ref } from "vue";
 
-const active = ref(0);
-
-const items = ref([
-    { label: "Users", icon: "pi pi-home" },
-    { label: "Authors", icon: "pi pi-chart-line" },
-    { label: "Admin", icon: "pi pi-list" },
-]);
+const usersData = usePage().props.users as Pagination<User>;
+const users = ref();
+onMounted(() => {
+    users.value = usersData.data as User[];
+});
 </script>
 <template>
-    <div>
-        <TabMenu v-model:activeIndex="active" :model="items">
-            <template #item="{ item, props }">
-                <a
-                    v-ripple
-                    v-bind="props.action"
-                    class="flex items-center gap-2"
-                >
-                    <span class="font-bold">{{ item.label }}</span>
-                </a>
-            </template>
-        </TabMenu>
+    <div class="card mt-4">
+        <DataTable :value="users" tableStyle="min-width: fit-content">
+            <Column field="id" header="Id" style="width: 3%"></Column>
+            <Column
+                field="name"
+                header="Name"
+                style="width: fit-content"
+            ></Column>
+        </DataTable>
+    </div>
+    <div class="w-full">
+        <div class="flex justify-center p-3 text-lg gap-3">
+            <Link
+                v-for="links in usersData.links"
+                v-html="links.label"
+                :href="
+                    links.url
+                        ? links.url
+                        : 'http://127.0.0.1:8000/dashboard?page=1'
+                "
+                class="hover:text-green-500"
+                :class="links.active ? 'text-red-500' : ''"
+            ></Link>
+        </div>
     </div>
 </template>
