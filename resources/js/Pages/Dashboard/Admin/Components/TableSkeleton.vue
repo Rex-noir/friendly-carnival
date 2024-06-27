ac1d90cf32e341825214afc6cf1c3cf40e22ab9a
AceRex <ace.rexnoir@gmail.com>
2024-06-27 09:32:54 +0530

    feat : add Table skeleton

---
+++ resources/js/Pages/Dashboard/Admin/Components/TableSkeleton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import { ref } from "vue";
+import Skeleton from "primevue/skeleton";
+import { fill } from "lodash";
+
+const products = ref(fill(Array(11), undefined));
+</script>
+<template>
+    <div class="card">
+        <DataTable :value="products">
+            <Column field="ID" header="ID">
+                <template #body>
+                    <Skeleton></Skeleton>
+                </template>
+            </Column>
+            <Column field="name" header="Name">
+                <template #body>
+                    <Skeleton></Skeleton>
+                </template>
+            </Column>
+        </DataTable>
+    </div>
+</template>
