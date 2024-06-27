31c9d5fb57568712bf9e37098439afe4963c84d2
AceRex <ace.rexnoir@gmail.com>
2024-06-27 09:32:24 +0530

    refactor : add skeleton and redesign the layout

---
+++ resources/js/Pages/Dashboard/Admin/Components/StatusSkeleton.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import Skeleton from "primevue/skeleton";
+import Card from "primevue/card";
+</script>
+<template>
+    <div
+        class="p-1 pb-20 flex flex-col gap-3 sm:grid sm:grid-cols-3 w-[95%] mx-auto"
+    >
+        <Card class="border dark:border-slate-700">
+            <template #title><span class="font-black"> Uptime </span></template>
+            <template #content> <Skeleton></Skeleton> </template>
+        </Card>
+        <Card class="border dark:border-slate-700">
+            <template #title
+                ><span class="font-black">CPU Status</span></template
+            >
+            <template #content>
+                <Skeleton></Skeleton>
+            </template>
+        </Card>
+        <Card class="border dark:border-slate-700">
+            <template #title
+                ><span class="font-black">Memory RAM</span></template
+            >
+            <template #content
+                ><div class="flex flex-col gap-3">
+                    <Skeleton></Skeleton>
+                    <Skeleton></Skeleton>
+                    <Skeleton></Skeleton>
+                </div>
+            </template>
+        </Card>
+        <Card class="border dark:border-slate-700">
+            <template #title
+                ><span class="font-black">Disk Status</span></template
+            >
+            <template #content
+                ><div class="flex flex-col gap-3">
+                    <Skeleton></Skeleton>
+                    <Skeleton></Skeleton>
+                    <Skeleton></Skeleton>
+                </div>
+            </template>
+        </Card>
+        <Card class="border dark:border-slate-700">
+            <template #title><span class="font-black">Database</span></template>
+            <template #content>
+                <Skeleton></Skeleton>
+            </template>
+        </Card>
+    </div>
+</template>
--- resources/js/Pages/Dashboard/Admin/Status.vue
+++ resources/js/Pages/Dashboard/Admin/Status.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import { SystemInfo, RealTimeSystemInfo } from "@/types/dashboard.interface";
-import { usePage } from "@inertiajs/vue3";
+import { RealTimeSystemInfo } from "@/types/dashboard.interface";
 import Card from "primevue/card";
-import ProgressSpinner from "primevue/progressspinner";
+import StatusSkeleton from "./Components/StatusSkeleton.vue";
 
 import { onBeforeUnmount, onMounted, ref } from "vue";
-
-const systemInfo = ref<SystemInfo>(usePage().props.systemStatus as SystemInfo);
-const { database, host, client, server } = systemInfo.value;
-const { hardware, software, uptime } = server;
 
 let realTimeInfo = ref<RealTimeSystemInfo | null>(null);
 
@@ -22,40 +17,27 @@ onBeforeUnmount(() => {
 });
 </script>
 <template>
+    <StatusSkeleton v-if="!realTimeInfo"></StatusSkeleton>
     <div
-        class="p-1 pb-20 flex flex-col gap-3 items-center sm:grid sm:grid-cols-3 w-[95%] place-items-center mx-auto"
+        v-if="realTimeInfo"
+        class="p-1 pb-20 flex flex-col gap-3 sm:grid sm:grid-cols-3 w-[95%] mx-auto"
     >
-        <Card
-            v-if="realTimeInfo"
-            class="border text-center dark:border-slate-700 h-fit md:w-fit"
-        >
-            <template #title
-                ><span class="font-black"
-                    >Uptime : {{ realTimeInfo.uptime }}</span
-                >
-            </template>
+        <Card class="border dark:border-slate-700">
+            <template #title><span class="font-black">Uptime </span> </template>
+            <template #content> {{ realTimeInfo.uptime }}</template>
         </Card>
-        <Card v-if="realTimeInfo" class="border dark:border-slate-700 md:w-fit">
+        <Card class="border dark:border-slate-700">
             <template #title
                 ><span class="font-black">CPU Status</span></template
             >
             <template #content>
                 <p>
                     <span class="font-bold">CPU Usage : </span>
-                    {{ realTimeInfo.cpu_load["15m"] }}%
-                </p>
-                <p><span class="font-bold">CPU : </span> {{ hardware.cpu }}</p>
-                <p>
-                    <span class="font-bold">CPU Cores : </span>
-                    {{ hardware.cpu_count }}
-                </p>
-                <p>
-                    <span class="font-bold">Virtualization : </span
-                    >{{ hardware.virtualization }}
+                    {{ realTimeInfo.cpu_load["1m"] }}%
                 </p>
             </template>
         </Card>
-        <Card v-if="realTimeInfo" class="border dark:border-slate-700 md:w-fit">
+        <Card class="border dark:border-slate-700">
             <template #title
                 ><span class="font-black">Memory RAM</span></template
             >
@@ -74,15 +56,7 @@ onBeforeUnmount(() => {
                 </p>
             </template>
         </Card>
-        <ProgressSpinner
-            v-if="!realTimeInfo"
-            style="width: 50px; height: 50px"
-            strokeWidth="3"
-            class="absolute fill-surface-0 col-start-2 dark:fill-surface-800"
-            animationDuration=".5s"
-            aria-label="Progressing"
-        />
-        <Card v-if="realTimeInfo" class="border dark:border-slate-700 md:w-fit">
+        <Card class="border dark:border-slate-700">
             <template #title
                 ><span class="font-black">Disk Status</span></template
             >
@@ -101,90 +75,13 @@ onBeforeUnmount(() => {
                 </p>
             </template>
         </Card>
-        <Card v-if="realTimeInfo" class="border dark:border-slate-700 md:w-fit">
-            <template #title><span class="font-black">Host</span></template>
-            <template #content>
-                <p>
-                    <span class="font-bold"> Hostname : </span>
-                    {{ host.hostname }}
-                </p>
-                <p>
-                    <span class="font-bold">IP : </span>
-                    {{ host.ip }}
-                </p>
-                <p>
-                    <span class="font-bold">Private IP : </span>
-                    {{ host.ip_private }}
-                </p>
-                <p>
-                    <span class="font-bold">Country : </span>
-                    {{ host.country }}
-                </p>
-                <p>
-                    <span class="font-bold">City : </span>
-                    {{ host.city }}
-                </p>
-                <p>
-                    <span class="font-bold">Location : </span>
-                    {{ host.location }}
-                </p>
-                <p>
-                    <span class="font-bold">Time Zone : </span>
-                    {{ host.timezone }}
-                </p>
-                <p>
-                    <span class="font-bold">Region : </span>
-                    {{ host.region }}
-                </p>
-                <p>
-                    <span class="font-bold">Postal : </span>
-                    {{ host.postal }}
-                </p>
-                <p>
-                    <span class="font-bold">Phone : </span>
-                    {{ host.phone }}
-                </p>
-            </template>
-        </Card>
-        <Card
-            v-if="realTimeInfo"
-            class="border dark:border-slate-700 md:w-fit h-fit"
-        >
+        <Card class="border dark:border-slate-700">
             <template #title><span class="font-black">Database</span></template>
             <template #content>
-                <p>
-                    <span class="font-bold"> Driver : </span>
-                    {{ database.driver }}
-                </p>
-                <p>
-                    <span class="font-bold">Version : </span>
-                    {{ database.version }}
-                </p>
                 <p>
                     <span class="font-bold">Size : </span>
                     {{ realTimeInfo.database }}
                 </p>
-            </template>
-        </Card>
-        <Card
-            v-if="realTimeInfo"
-            class="border dark:border-slate-700 md:w-fit h-fit sm:col-start-2"
-        >
-            <template #title><span class="font-black">OS</span></template>
-            <template #content>
-                <p>
-                    <span class="font-bold">Name : </span>
-                    {{ software.os }}
-                </p>
-                <p>
-                    <span class="font-bold">Distro : </span>
-                    {{ software.distro }}
-                </p>
-                <p>
-                    <span class="font-bold">Kernel : </span>
-                    {{ software.kernel }}
-                </p>
-                <p><span class="font-bold">Arch : </span>{{ software.arc }}</p>
             </template>
         </Card>
     </div>
