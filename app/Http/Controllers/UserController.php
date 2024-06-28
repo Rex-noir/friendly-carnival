6641d3420b1191dbf337cb1ec0830fae1216726f
AceRex <ace.rexnoir@gmail.com>
2024-06-28 22:04:24 +0530

    refactor : use UserPolicy

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
@@ -12,6 +13,7 @@ use function Pest\Laravel\json;
 
 class UserController extends Controller
 {
+    use AuthorizesRequests;
     /**
      * Display a listing of the resource.
      */
@@ -58,25 +60,22 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user, $id)
     {
-        if (Auth::user()->role !== 'admin' && Auth::user()->id !== $id) {
-            return response()->json(['errors' => 'Authorization error'], 403);
-        }
-        try {
-            $validated = $request->validate([
-                'email' => ['string', 'email', 'max:255'],
-                'name' => ['string', 'max:255',]
-            ]);
+        $this->authorize('update', $user);
+        $validator = validator()->make(
+            $request->all(),
+            ['email' => ['string', 'email', 'max:255'], 'name' => ['string', 'max:255']]
+        );
 
-            User::findOrFail($id)->update($request->only('email', 'name'));
-            return response($validated);
-        } catch (\Throwable $th) {
-            if ($th instanceof ModelNotFoundException) {
-                return response()->json(['errors' => 'User not found'], 404);
-            } else if ($th instanceof ValidationException) {
-                return response()->json(['errors' => $th->errors(), 'email' => User::find($id)->email], 422);
-            }
-            return response()->json(['errors' => "Something's wrong"], 500);
+        $userToUpdate = User::findOrFail($id);
+
+        //validator fails
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors(), 'email' => $userToUpdate->email], 422);
         }
+        //Update
+        $userToUpdate->update($validator->validated());
+
+        return response()->json(['message' => "User updated successfully"]);
     }
 
     /**
@@ -84,21 +83,10 @@ class UserController extends Controller
      */
     public function destroy(User $user, $id)
     {
-        try {
-            if (Auth::user()->role === 'user' && Auth::user()->id === $id) {
-                User::findOrFail($id)->delete();
-                return response()->json(null, 204);
-            } else if (Auth::user()->role === 'admin') {
-                User::findOrFail($id)->delete();
-                return response()->json(null, 204);
-            }
-            return response()->json(['errors' => 'Authorization error', 403]);
-        } catch (\Throwable $th) {
-            if ($th instanceof ModelNotFoundException) {
-                return response()->json(['errors' => 'User not found'], 404);
-            }
-            return response()->json(['errors' => "Something's wrong"], 500);
-        }
+        $userToDelete = User::findOrFail($id);
+        $this->authorize('delete', $userToDelete);
+        $userToDelete->delete();
+        return response()->json(null, 204);
     }
 
     /** Search users */
