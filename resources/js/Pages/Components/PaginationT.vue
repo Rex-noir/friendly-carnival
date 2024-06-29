21afd3d0b253d5847aee728d59102a4e66456b44
Lala <ace.rexnoir@gmail.com>
2024-06-29 16:39:56 +0530

    refactor : use Sven Walbröl's Paginator

--- resources/js/Pages/Components/PaginationT.vue
+++ resources/js/Pages/Components/PaginationT.vue
@@ -1,85 +1,224 @@
 <script setup lang="ts">
-import { Pagination, User } from "@/types/page.interfaces";
+import { Pagination } from "@/types/page.types";
 import Button from "primevue/button";
-import { ref } from "vue";
-
+import { computed } from "vue";
 const props = defineProps<{ paginator: Pagination<User[]> }>();
+const { paginator } = props;
 
-const totalPages = props.paginator.last_page;
-const currentPage = props.paginator.current_page;
-const numLinksAround = 2;
+function isFirstOrLastOrDots(
+    index: number,
+    links_length: number,
+    label: string
+) {
+    return index === 0 || index === links_length - 1 || label.includes("...");
+}
 
-const startPage = Math.max(1, currentPage - numLinksAround);
-const endPage = Math.min(totalPages, currentPage + numLinksAround);
-const displayedLinks = ref(
-    props.paginator.links
-        .filter((link, index) => Number(link.label) === index)
-        .slice(startPage - 1, endPage)
+const onFirstPage = computed(() => paginator.current_page === 1);
+
+const hasMorePages = computed(
+    () => paginator.current_page < paginator.last_page
+);
+
+const nextPageUrl = computed(() => paginator.next_page_url);
+
+const previousPageUrl = () => paginator.prev_page_url;
+const displayLinks = computed(() =>
+    props.paginator.links.filter((link, index) => Number(link.label) === index)
 );
 </script>
+
 <template>
-    <nav aria-label="Page navigation">
-        <ul class="flex gap-1 items-center -space-x-px h-8 text-sm">
-            <li>
+    <div class="flex justify-center flex-col items-center gap-2">
+        <div>
+            <p>
+                Showing <span class="font-medium">{{ paginator.from }}</span> to
+                <span class="font-medium">{{ paginator.to }}</span> of
+                <span class="font-medium">{{ paginator.total }}</span> results
+            </p>
+        </div>
+        <nav
+            v-if="paginator !== undefined"
+            class="flex items-center justify-between"
+            role="navigation"
+        >
+            <div class="flex justify-between flex-1 sm:hidden">
+                <Button
+                    v-if="onFirstPage"
+                    class="relative inline-flex items-center cursor-pointer px-4 py-2 text-sm font-medium text-gray-500 bg-gray-100 border border-gray-300 leading-5 rounded-md"
+                >
+                    <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path
+                            clip-rule="evenodd"
+                            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                            fill-rule="evenodd"
+                        />
+                    </svg>
+                </Button>
                 <Button
                     @click="$emit('paginate', paginator.prev_page_url)"
-                    class="flex items-center focus:!ring-0 justify-center px-3 h-8 leading-tight text-surface-900 !bg-gray-200 border !border-gray-300 hover:!bg-gray-100 hover:!text-gray-700 dark:!bg-gray-800 dark:!border-gray-700 dark:!text-gray-400 dark:hover:!bg-gray-700 dark:!hover:text-white"
+                    v-else
+                    :href="previousPageUrl"
+                    class="relative cursor-pointer inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
                 >
-                    <span class="sr-only">Previous</span>
                     <svg
-                        class="w-2.5 h-2.5 rtl:rotate-180"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 6 10"
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
                     >
                         <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M5 1 1 5l4 4"
+                            clip-rule="evenodd"
+                            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                            fill-rule="evenodd"
                         />
                     </svg>
                 </Button>
-            </li>
-            <li class="flex items-center gap-1 justify-center">
                 <Button
-                    v-for="link in displayedLinks"
-                    @click="$emit('paginate', link.url)"
-                    :class="
-                        Number(link.label) === paginator.current_page
-                            ? ' !text-red-500'
-                            : ''
-                    "
-                    class="flex items-center focus:!ring-0 justify-center px-3 h-8 leading-tight text-surface-900 !bg-gray-200 border !border-gray-300 hover:!bg-gray-100 hover:!text-gray-700 dark:!bg-gray-800 dark:!border-gray-700 dark:!text-gray-400 dark:hover:!bg-gray-700 dark:!hover:text-white"
+                    @click="$emit('paginate', nextPageUrl)"
+                    v-if="hasMorePages"
+                    :href="nextPageUrl"
+                    class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
                 >
-                    {{ link.label }}
-                </Button>
-            </li>
-            <li>
-                <Button
-                    @click="$emit('paginate', paginator.next_page_url)"
-                    class="flex items-center focus:!ring-0 justify-center px-3 h-8 leading-tight text-surface-900 !bg-gray-200 border !border-gray-300 hover:!bg-gray-100 hover:!text-gray-700 dark:!bg-gray-800 dark:!border-gray-700 dark:!text-gray-400 dark:hover:!bg-gray-700 dark:!hover:text-white"
-                >
-                    <span class="sr-only">Next</span>
                     <svg
-                        class="w-2.5 h-2.5 rtl:rotate-180"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 6 10"
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
                     >
                         <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="m1 9 4-4-4-4"
+                            clip-rule="evenodd"
+                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                            fill-rule="evenodd"
                         />
                     </svg>
                 </Button>
-            </li>
-        </ul>
-    </nav>
+                <Button
+                    v-else
+                    class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-500 bg-gray-100 border border-gray-300 cursor-default leading-5 rounded-md"
+                >
+                    <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path
+                            clip-rule="evenodd"
+                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                            fill-rule="evenodd"
+                        />
+                    </svg>
+                </Button>
+            </div>
+            <div
+                class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between"
+            >
+                <div class="flex gap-2">
+                    <span
+                        class="relative z-0 inline-flex gap-2 shadow-sm rounded-md"
+                    >
+                        <Button
+                            v-if="onFirstPage"
+                            aria-disabled="true"
+                            aria-hidden="true"
+                            class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-gray-100 border border-gray-300 cursor-default rounded-l-md leading-5"
+                        >
+                            <svg
+                                class="w-5 h-5"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    clip-rule="evenodd"
+                                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                    fill-rule="evenodd"
+                                />
+                            </svg>
+                        </Button>
+                        <Button
+                            v-else
+                            @click="$emit('paginate', previousPageUrl)"
+                            class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"
+                            rel="prev"
+                        >
+                            <svg
+                                class="w-5 h-5"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    clip-rule="evenodd"
+                                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                    fill-rule="evenodd"
+                                />
+                            </svg>
+                        </Button>
+                        <div class="flex gap-2">
+                            <div v-for="link in displayLinks">
+                                <Button
+                                    v-if="
+                                        !isFirstOrLastOrDots(
+                                            paginator.current_page,
+                                            paginator.links.length,
+                                            link.label
+                                        )
+                                    "
+                                    :class="{
+                                        'bg-blue-200': link.active === true,
+                                    }"
+                                    @click="$emit('paginate', link.url)"
+                                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                                >
+                                    {{ link.label }}
+                                </Button>
+                                <span
+                                    v-else-if="link.label === '...'"
+                                    aria-disabled="true"
+                                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 cursor-default leading-5"
+                                >
+                                    {{ link.label }}
+                                </span>
+                            </div>
+                        </div>
+                        <Button
+                            v-if="hasMorePages"
+                            @click="$emit('paginate', nextPageUrl)"
+                            class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"
+                        >
+                            <svg
+                                class="w-5 h-5"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    clip-rule="evenodd"
+                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                    fill-rule="evenodd"
+                                />
+                            </svg>
+                        </Button>
+                        <Button
+                            v-else
+                            aria-disabled="true"
+                            aria-hidden="true"
+                            class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-gray-100 border border-gray-300 cursor-default rounded-r-md leading-5"
+                        >
+                            <svg
+                                class="w-5 h-5"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    clip-rule="evenodd"
+                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                    fill-rule="evenodd"
+                                />
+                            </svg>
+                        </Button>
+                    </span>
+                </div>
+            </div>
+        </nav>
+    </div>
 </template>
