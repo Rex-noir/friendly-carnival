c37265002622f960158c769fecc465fe216cd21e
Lala <ace.rexnoir@gmail.com>
2024-06-29 15:32:49 +0530

    refactor : declare user globally feat : add status to the user interface

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,9 +1,20 @@
 import Echo from "laravel-echo";
 import { route as routeFn } from "ziggy-js";
+import { UserStatus } from "./actions.types";
 
 declare global {
     interface Window {
         Echo: Echo;
     }
     var route: typeof routeFn;
+    interface User {
+        id: number;
+        name: string;
+        email: string;
+        email_verified_at: string;
+        created_at: string;
+        updated_at: string;
+        role: string;
+        status: UserStatus;
+    }
 }
