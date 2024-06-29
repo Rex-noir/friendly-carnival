450f5ae7940a3cc836d907e20eeb76882c387c87
Lala <ace.rexnoir@gmail.com>
2024-06-29 15:34:25 +0530

    feat : add test for status updating refactor : implement actingAs to reflect policies

--- tests/Feature/UserUpdateTest.php
+++ tests/Feature/UserUpdateTest.php
@@ -15,15 +15,24 @@ class UserUpdateTest extends TestCase
      * A basic feature test example.
      */
 
+    protected $adminUser;
+
+    protected function  setUp(): void
+    {
+        parent::setUp();
+        $admin = User::factory()->create(['role' => 'admin']);
+        $this->adminUser = $admin;
+    }
+
+
     public function test_Update_user()
     {
         $user = User::factory()->create();
 
         $newData = ['email' => 'kaihpi@example.com'];
 
-        $response = $this->put(route('users.update', $user->id), $newData);
-        $response->assertStatus(200);
-        $response->assertJson($newData);
+        $response = $this->actingAs($this->adminUser)->put(route('users.update', $user->id), $newData);
+        $response->assertStatus(204);
 
         $this->assertDatabaseHas('users', ['id' => $user->id, 'email' => 'kaihpi@example.com', 'name' => $user->name]);
     }
@@ -33,9 +42,16 @@ class UserUpdateTest extends TestCase
         $user = User::factory()->create();
 
         $newData = ['email' => 'kulikawn@example.com', 'name' => 'Suanga'];
-        $response = $this->put(route('users.update', $user->id), $newData);
-        $response->assertStatus(200);
-        $response->assertJson($newData);
+        $response = $this->actingAs($this->adminUser)->put(route('users.update', $user->id), $newData);
+        $response->assertStatus(204);
         $this->assertDatabaseHas('users', ['id' => $user->id, 'email' => $newData['email'], 'name' => $newData['name']]);
+    }
+
+    public function test_Update_status()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($this->adminUser)->post(route('users.ban', $user->id));
+        $response->assertStatus(204);
+        $this->assertDatabaseHas('users', ['id' => $user->id, 'status' => 'banned']);
     }
 }
