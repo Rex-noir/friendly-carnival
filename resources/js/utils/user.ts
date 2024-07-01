4591dfab782452a7a638ba393706a7d0153f505b
Lala <ace.rexnoir@gmail.com>
2024-07-01 10:22:58 +0530

    feat : add filtering logic refactor : merge filtering method with indexing

--- resources/js/utils/user.ts
+++ resources/js/utils/user.ts
@@ -1,3 +1,5 @@
+import { filterConfig } from "@/Pages/Components/RoleFilter.vue";
+import { UserRoles, UserStatus } from "@/types/enums.types";
 import { Pagination } from "@/types/page.types";
 import axios from "axios";
 
@@ -17,16 +19,27 @@ export default class UserUtils {
             throw error;
         }
     }
-
     /**
-     * Fetches a paginated list of users from the specified URL or default route.
+     * Fetches a paginated list of users from the specified URL or default route,
+     * with an optional filter configuration.
      * @param url Optional. The URL to fetch data from. If not provided, defaults to the route "users.index".
+     * @param filterConfig Optional. An object containing filter configuration.
      * @returns A promise that resolves to a Pagination object containing an array of User objects.
      * @throws Throws an error if the HTTP request fails or returns a non-200 status code.
      */
-    static async index(url?: string): Promise<Pagination<User[]>> {
+    static async fetchUsers(
+        url?: string,
+        filterConfig?: filterConfig,
+    ): Promise<Pagination<User[]>> {
         try {
-            const response = await axios.post(url || route("users.index"));
+            const params: Record<string, any> = {};
+            if (filterConfig) {
+                params.role = filterConfig.role;
+                params.status = filterConfig.status;
+            }
+            const response = await axios.get(url || route("users.index"), {
+                params,
+            });
             return response.data;
         } catch (error) {
             throw error;
@@ -43,7 +56,7 @@ export default class UserUtils {
         try {
             const response = await axios.put(
                 route("users.update", { id: info.id }),
-                info
+                info,
             );
             return response.data;
         } catch (error) {
@@ -60,17 +73,24 @@ export default class UserUtils {
      */
     static async delete(id: number): Promise<void> {
         try {
-            const response = await axios.delete(
-                route("users.delete", { id: id })
-            );
+            await axios.delete(route("users.delete", { id: id }));
         } catch (error) {
             throw error;
         }
     }
 
+    /**
+     * Bans a user by their ID.
+     *
+     * This method sends a POST request to the server to ban a user identified by the provided ID.
+     *
+     * @param {number} id - The ID of the user to ban.
+     * @returns {Promise<void>} A promise that resolves when the user has been successfully banned.
+     * @throws Will throw an error if the request fails.
+     */
     static async ban(id: number): Promise<void> {
         try {
-            const response = await axios.post(route("users.ban", { id: id }));
+            await axios.post(route("users.ban", { id: id }));
         } catch (error) {
             throw error;
         }
