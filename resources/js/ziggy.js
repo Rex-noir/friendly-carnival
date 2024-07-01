beef8ad49d4395d76d6364658e5d1058cb918bba
Lala <ace.rexnoir@gmail.com>
2024-07-01 10:24:04 +0530

    refactor : use get instead of post when indexing lists

--- resources/js/ziggy.js
+++ resources/js/ziggy.js
@@ -1,4 +1,4 @@
-const Ziggy = {"url":"http:\/\/localhost","port":null,"defaults":{},"routes":{"dashboard":{"uri":"dashboard","methods":["GET","HEAD"]},"users.search":{"uri":"users\/search","methods":["POST"]},"users.index":{"uri":"users\/index","methods":["POST"]},"users.update":{"uri":"users\/update\/{id}","methods":["PUT"],"parameters":["id"]},"users.delete":{"uri":"users\/delete\/{id}","methods":["DELETE"],"parameters":["id"]},"users.ban":{"uri":"users\/ban\/{id}","methods":["POST"],"parameters":["id"]}}};
+const Ziggy = {"url":"http:\/\/localhost","port":null,"defaults":{},"routes":{"dashboard":{"uri":"dashboard","methods":["GET","HEAD"]},"users.search":{"uri":"users\/search","methods":["POST"]},"users.index":{"uri":"users\/index","methods":["GET","HEAD"]},"users.update":{"uri":"users\/update\/{id}","methods":["PUT"],"parameters":["id"]},"users.delete":{"uri":"users\/delete\/{id}","methods":["DELETE"],"parameters":["id"]},"users.ban":{"uri":"users\/ban\/{id}","methods":["POST"],"parameters":["id"]}}};
 if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
     Object.assign(Ziggy.routes, window.Ziggy.routes);
 }
